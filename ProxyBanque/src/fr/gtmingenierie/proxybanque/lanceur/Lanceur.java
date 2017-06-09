1d5022204b54f1698fed293c7d04ac83f631f443
Clement Roux <roux-clement@laposte.net>
2017-06-09 14:48:49 +0200

    - Correctif pour le lanceur

--- ProxyBanque/src/fr/gtmingenierie/proxybanque/lanceur/Lanceur.java
+++ ProxyBanque/src/fr/gtmingenierie/proxybanque/lanceur/Lanceur.java
@@ -39,18 +39,18 @@ public class Lanceur {
 		Menu menu = new Menu(agenceService, conseillerService, clientService, compteService, transactionService);
 		
 		// Remplissage des différentes listes
-		agenceService.ajoutAgence("ABCD1", new Agence("08/06/2017"));
-		agenceService.ajoutAgence("ABCD2", new Agence("08/06/2017"));
+		agenceService.ajoutAgence("00000", new Agence("08/06/2017"));
+		agenceService.ajoutAgence("00001", new Agence("08/06/2017"));
 		
 		gerantService.ajoutGerant(new Gerant("Wayne", "Bruce", "batman"));
 		gerantService.ajoutGerant(new Gerant("Stark", "Tony", "ironman"));
 		
-		conseillerService.ajoutConseiller("ABCD1", new Conseiller("Hubert", "Paul", "password"));
-		conseillerService.ajoutConseiller("ABCD2", new Conseiller("Dupont", "Michel", "password"));
+		conseillerService.ajoutConseiller("00000", new Conseiller("Hubert", "Paul", "password"));
+		conseillerService.ajoutConseiller("00001", new Conseiller("Dupont", "Michel", "password"));
 		
-		clientService.ajoutClient("ABCD1", 0, new Client("Bad", "Pittr", "Hollywood Boulevard", "38000", "Los Angeles", "04 05 60 78 21"));
-		clientService.ajoutClient("ABCD1", 0, new Client("Clooney", "Georges", "Sunset Avenu", "38000", "Los Angeles", "04 05 60 78 21"));
-		clientService.ajoutClient("ABCD2", 0, new Client("Diamond", "Matt", "Time Square", "38000", "New York", "04 05 60 78 21"));
+		clientService.ajoutClient("00000", 0, new Client("Bad", "Pittr", "Hollywood Boulevard", "38000", "Los Angeles", "04 05 60 78 21"));
+		clientService.ajoutClient("00000", 0, new Client("Clooney", "Georges", "Sunset Avenu", "38000", "Los Angeles", "04 05 60 78 21"));
+		clientService.ajoutClient("00001", 0, new Client("Diamond", "Matt", "Time Square", "38000", "New York", "04 05 60 78 21"));
 		
 		System.out.println("Numéros des comptes associés aux bourses étrangères :");
 		System.out.println("Paris : "+paris.getNum());
@@ -58,19 +58,19 @@ public class Lanceur {
 		System.out.println("New York : "+newYork.getNum());
 		
 		boolean temp;
-		temp = compteService.creerCompteCourant("ABCD1", 2, 0, 10000.0, "09/06/2017");
-		temp = compteService.creerCompteCourant("ABCD1", 2, 1, 5000.0, "09/06/2017");
-		temp = compteService.creerCompteCourant("ABCD2", 3, 0, 150000.0, "09/06/2017");
+		temp = compteService.creerCompteCourant("00000", 2, 0, 10000.0, "09/06/2017");
+		temp = compteService.creerCompteCourant("00000", 2, 1, 5000.0, "09/06/2017");
+		temp = compteService.creerCompteCourant("00001", 3, 0, 150000.0, "09/06/2017");
 		// Assignation des gérants dans les agences (possibilité de faire une boucle)
-		agenceService.assignerGerant("ABCD1", gerantService.getGerant(0));
-		agenceService.assignerGerant("ABCD2", gerantService.getGerant(1));
+		agenceService.assignerGerant("00000", gerantService.getGerant(0));
+		agenceService.assignerGerant("00001", gerantService.getGerant(1));
 		
 		// Affichage des listes
 		agenceService.afficher();
-		conseillerService.afficher("ABCD1");
-		clientService.afficher("ABCD1", 0);
-		conseillerService.afficher("ABCD2");
-		clientService.afficher("ABCD2", 0);
+		conseillerService.afficher("00000");
+		clientService.afficher("00000", 0);
+		conseillerService.afficher("00001");
+		clientService.afficher("00001", 0);
 		
 		// Menu
 		menu.useMenu();
